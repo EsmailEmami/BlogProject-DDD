3e4654e48896497c6d8c1511c0dc3c08f40f7e81
EsmailEmami <esmailemami84@gmail.com>
2022-05-03 11:27:05 +0430

    Add Category Entity

---
+++ src/Blog.Domain/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Domain.Core.Models;
+using Dapper.Contrib.Extensions;
+
+namespace Blog.Domain.Models;
+
+[Table("[Category].[Categories]")]
+public class Category : Entity
+{
+    public Category(Guid id, string categoryTitle)
+    {
+        Id = id;
+        CategoryTitle = categoryTitle;
+    }
+
+    public string CategoryTitle { get; private set; }
+}
